e8944ed08032ffc5e4e865e37e1e29b13d1864c3
tiankonguse <i@tiankonguse.com>
2014-03-30 13:04:31 +0800

    update margin

--- aboutme.php
+++ aboutme.php
@@ -23,10 +23,13 @@ TK.loader.loadCSS({url:"<?php echo MAIN_PATH;?>css/main.css"});
                 <div class="container about-me">
                     <h1 class="entry-title">关于本站</h1>
                     <div class="entry-content">
-                        <p>本站是tiankonguse 的个人网站。</p>
-                        <p>一篇文章在互联网上称为博客，但是，tiankonguse 把它称为一个记录。</p>
-                        <p>tiankonguse 的记录通常会记录下 tiankonguse 的生活感想，学习过程，所见所闻和看到的好文章等。</p>
-                        <p>这里的记录自 2014年起，列表按最近修改时间来排序。</p>
+                    <p>这里是 <a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 的个人网站。</p>
+                        <p>一篇文章在互联网上称为博客，但是，<a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 把它称为一个记录。</p>
+                        <p><a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 的记录通常会记录下 <a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 的生活感想，学习过程，所见所闻和看到的好文章等。</p>
+                        <P>关于这个网站的源代码，全部托管在 <a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 的 <a href="http://github.com/tiankonguse/record/">github</a>上  </p>
+                        <p> <a href="<?php echo MAIN_PATH;?>" >tiankonguse </a> 的这个 <a href="<?php echo MAIN_PATH;?>" >record </a> 网站算是纯手工敲出来的，所以避免不了有漏洞,bug。从网站的外观也可以看出来，我是一个屌丝。屌丝的审美观和大众确实不太一样。<br> 所以你有什么好的想法可以告诉我，或者我们一起实现。</p>
+                        <p>欢迎向我提交友情链接。</p>
+
                         <p>下面的是 tiankonguse 的新浪微博。</p>
                         <p>
                             <iframe width="100%" height="550" class="share_self"
