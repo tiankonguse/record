64bd813e13bf968ab217d96c6794d8dc7c93c62f
tiankonguse <i@tiankonguse.com>
2014-04-19 13:04:21 +0800

    -A

--- fix.php
+++ fix.php
@@ -1,32 +1,146 @@
 <?php
-session_start();
-require("./inc/common.php");
-require("./inc/function.php");
+ignore_user_abort (); // 关掉浏览器，PHP脚本也可以继续执行.
+set_time_limit ( 0 ); // 通过set_time_limit(0)可以让程序无限制的执行下去
+ini_set ( "display_errors", 1 );
+ini_set ( "error_reporting", E_ALL );
+session_start ();
+require ("../common/inc/init.php");
 
-
-/* 
- * 为 record 添加了最后修改时间字段
- */
-if(0){
-	$sql = "select * from `record_record`";
-	$result = mysql_query($sql ,$conn);
-	while($row = mysql_fetch_array($result)){
-		$id = $row['id'];
-		$last_time = $row["time"];
-		$sql = "UPDATE `record_record` SET `last_time`= `time` WHERE `id` = '$id'";
-		mysql_query($sql ,$conn);
-	}
-
-	mysql_query("UPDATE `record_record` SET `last_time`= `time`" ,$conn);
-
-}
+$flag = 2;
 
 /*
- * 由于为 tag 添加了一个数量的字段，所以写了一个自动更新的SQL 语句。
+ * 为 record 添加了最后修改时间字段
  */
-if(0){
-    $sql = "update record_tag t1 set number = (select count(*) num from record_tag_map  t2 where t2.tag_id = t1.id)";
+
+if ($flag == 0) {
+} else if ($flag == 1) {
+	/*
+	 * 由于为 tag 添加了一个数量的字段，所以写了一个自动更新的SQL 语句。
+	 */
+} else if ($flag == 2) {
+	// 先把所有的tag写到wp
+	
+	fix_record_to_wp ();
+	
+	//
+	// wp_terms(term_id, name, slug)
+	// auto 'name' urldecode 0
+	//
+	//
+	// wp_term_taxonomy
+	// (term_taxonomy_id, term_id, taxonomy, count)
+	// auto term_id "post_tag" number
+	//
+	// wp_term_relationships(object_id, term_taxonomy_id, term_order)
+	// post_id term_taxonomy_id 0
+	//
+	// wp_posts(post_author, post_date, post_date_gmt, post_content, post_titlepost_modified, post_modified_gmt
+	//
+} else if ($flag == 3) {
+} else if ($flag == 4) {
+} else {
 }
 
 echo "finish \n";
-
+function fix_record_to_wp() {
+	global $conn;
+	$result_record = mysql_query ( "select id, title, content, time, last_time from record_record", $conn );
+	while ( $row_record = mysql_fetch_array ( $result_record ) ) {
+		$record_id = $row_record ["id"];
+		$record_title = $row_record ["title"];
+		$record_content = $row_record ["content"];
+		$record_time = $row_record ["time"];
+		$record_last_time = $row_record ["last_time"];
+		
+		var_dump ( "$record_title begin", "<br>" );
+		
+		$dtms = new DateTime ();
+		
+		$wp_post_author = "1";
+		$wp_post_date = $dtms->setTimestamp ( $record_time )->format ( "Y-m-d H:i:s.000000" );
+		$wp_post_date_gmt = gmdate ( "Y-m-d H:i:s.000000", $record_time );
+		$wp_post_content = mysql_real_escape_string ( $record_content );
+		$wp_post_title = mysql_real_escape_string ( $record_title );
+		$wp_post_name = urlencode ( $wp_post_title );
+		$wp_post_modified = $dtms->setTimestamp ( $record_last_time )->format ( "Y-m-d H:i:s.000000" );
+		$wp_post_modified_gmt = gmdate ( "Y-m-d H:i:s.000000", $record_last_time );
+		
+		var_dump ( "begin select $record_title from wp_posts", "<br>" );
+		
+		$result_post = mysql_query ( "select * from wp_posts where post_title = '" . $wp_post_title . "'", $conn );
+		if ($row_post = mysql_fetch_array ( $result_post )) {
+			$wp_post_id = $row_post ["ID"];
+			var_dump ( "find $record_title, id is $wp_post_id ,content length is " . strlen ( $row_post ["post_content"] ), "<br>" );
+			if (strcmp ( $row_post ["post_content"], "" ) == 0) {
+				$sql = "update wp_posts set post_content = '" . $wp_post_content . "', post_name = '" . $wp_post_name . "' and guid = 'http://tiankonguse.com/blog/?p=$wp_post_id' where ID = '" . $wp_post_id . "'";
+				$ret = mysql_query ( $sql, $conn );
+			}
+		} else {
+			mysql_query ( "INSERT INTO wp_posts(post_author, post_date, post_date_gmt, post_content, post_title, post_modified, post_modified_gmt) VALUES('" . $wp_post_author . "','" . $wp_post_date . "','" . $wp_post_date_gmt . "','" . $wp_post_content . "','" . $wp_post_title . "','" . $wp_post_modified . "','“.$wp_post_modified_gmt.”')", $conn );
+			var_dump ( "insert " . $record_title, "<br>" );
+			$wp_post_id = mysql_insert_id ();
+		}
+		
+		add_record_tags_to_wp ( $wp_post_id, $record_id );
+	}
+}
+function add_record_tags_to_wp($wp_post_id, $record_id) {
+	global $conn;
+	var_dump ( "begin select " . $record_id . " 's tags to " . $wp_post_id, "<br>" );
+	$result_tag_name = mysql_query ( "select name, number from record_tag where id in (select tag_id from record_tag_map where record_id = '" . $record_id . "')", $conn );
+	while ( $row = mysql_fetch_array ( $result_tag_name ) ) {
+		if (strcmp ( $row ["name"], "" ) != 0) {
+			var_dump ( "find a tag:" . $row ["name"] . " number:" . $row ["number"], "<br>" );
+			$wp_taxonomy_id = get_wp_taxonomy_id ( $row ["name"], $row ["number"] );
+			var_dump ( "find wp_taxonomy_id:" . $wp_taxonomy_id, "<br>" );
+			wp_map_taxonomy_post ( $wp_post_id, $wp_taxonomy_id );
+		}
+	}
+}
+function wp_map_taxonomy_post($wp_post_id, $wp_taxonomy_id) {
+	global $conn;
+	$result = mysql_query ( "select * from wp_term_relationships where object_id = '" . $wp_post_id . "' and term_taxonomy_id = '" . $wp_taxonomy_id . "'", $conn );
+	if (mysql_num_rows ( $result ) == 0) {
+		mysql_query ( "INSERT INTO wp_term_relationships(object_id, term_taxonomy_id)VALUES ('" . $wp_post_id . "', '" . $wp_taxonomy_id . "')", $conn );
+	}
+}
+function get_wp_taxonomy_id($tag_name, $count) {
+	global $conn;
+	$term_id = get_wp_term_id ( $tag_name );
+	var_dump ( "get_wp_taxonomy_id get tag $tag_name 's id : $term_id", "<br>" );
+	
+	// 查看 term_taxonomy_id, 没有则插入新的记录
+	
+	$result_taxonomy = mysql_query ( "select * from wp_term_taxonomy where term_id = '" . $term_id . "' and taxonomy = 'post_tag'", $conn );
+	
+	if ($row_terms = mysql_fetch_array ( $result_taxonomy )) {
+		$term_taxonomy_id = $row_terms ["term_taxonomy_id"];
+	} else {
+		mysql_query ( "INSERT INTO wp_term_taxonomy(term_id, taxonomy, count) VALUES ('" . $term_id . "', 'post_tag', '" . $count . "')", $conn );
+		
+		$term_taxonomy_id = mysql_insert_id ();
+	}
+	return $term_taxonomy_id;
+}
+function get_wp_term_id($tag_name) {
+	global $conn;
+	$slug = urlencode ( $tag_name );
+	$slug = mysql_real_escape_string ( $slug );
+	$tag_name = mysql_real_escape_string ( $tag_name );
+	// 得到 term_id, 没有则插入新的记录。
+	var_dump ( "tag_name:" . $tag_name . " slug:" . $slug, "<br>" );
+	
+	$result_terms = mysql_query ( "select * from wp_terms where name = '" . $tag_name . "'", $conn );
+	
+	if ($row_terms = mysql_fetch_array ( $result_terms )) {
+		$term_id = $row_terms ["term_id"];
+		var_dump ( "得到term_id " . $term_id . ", 没有则插入新的记录", "<br>" );
+	} else {
+		var_dump ( $slug . "： " . $tag_name, "<br>" );
+		$sql = "INSERT INTO wp_terms(name, slug) VALUES ('" . $tag_name . "', '" . $slug . "')";
+		$result = mysql_query ( $sql, $conn );
+		$term_id = mysql_insert_id ();
+		var_dump ( "未的到term_id " . $term_id . ", 插入新的记录", "<br>" );
+	}
+	return $term_id;
+}
--- inc/function.php
+++ inc/function.php
@@ -1,274 +1,262 @@
 <?php
-
-function getRecordNum(){
+function getRecordNum() {
 	global $conn;
-	//操作数据库
+	// 操作数据库
 	$sql = "select count(*) num from `record_record`";
-	$result = mysql_query($sql ,$conn);
-	$row= mysql_fetch_array($result);
-	return $row['num'];
+	$result = mysql_query ( $sql, $conn );
+	$row = mysql_fetch_array ( $result );
+	return $row ['num'];
 }
-function getRecordNumByTagId($tagId){
+function getRecordNumByTagId($tagId) {
 	global $conn;
-	//操作数据库
+	// 操作数据库
 	$sql = "select count(*) num from `record_tag_map` where tag_id = '$tagId'";
-	$result = mysql_query($sql ,$conn);
-	$row= mysql_fetch_array($result);
-	return $row['num'];
+	$result = mysql_query ( $sql, $conn );
+	$row = mysql_fetch_array ( $result );
+	return $row ['num'];
 }
-
-function getTagId($tag){
+function getTagId($tag) {
 	global $conn;
-	//操作数据库
+	// 操作数据库
 	$sql = "select id from `record_tag` where name = '$tag'";
-	$result = mysql_query($sql ,$conn);
-	$row= mysql_fetch_array($result);
-	return $row['id'];
+	$result = mysql_query ( $sql, $conn );
+	$row = mysql_fetch_array ( $result );
+	return $row ['id'];
 }
-
-function initTagPage($tag, $pageSize){
-	$_GET['pageSize'] = $pageSize;
-	$tagId = getTagId($tag);
-	$_GET['tagId'] = $tagId;
-
-	$allPageNum = intval((getRecordNumByTagId($tagId) + $pageSize - 1) / $pageSize);
-	$_GET['allPageNum'] = $allPageNum;
-
+function initTagPage($tag, $pageSize) {
+	$_GET ['pageSize'] = $pageSize;
+	$tagId = getTagId ( $tag );
+	$_GET ['tagId'] = $tagId;
+	
+	$allPageNum = intval ( (getRecordNumByTagId ( $tagId ) + $pageSize - 1) / $pageSize );
+	$_GET ['allPageNum'] = $allPageNum;
+	
 	$nowPage = 1;
-	if(isset($_GET['nowPage'])){
-		$nowPage = intval($_GET['nowPage']);
+	if (isset ( $_GET ['nowPage'] )) {
+		$nowPage = intval ( $_GET ['nowPage'] );
 	}
-	if($nowPage <= 0){
+	if ($nowPage <= 0) {
 		$nowPage = 1;
-	}else if($nowPage > $allPageNum){
+	} else if ($nowPage > $allPageNum) {
 		$nowPage = $allPageNum;
 	}
-	$_GET['nowPage'] = $nowPage;
+	$_GET ['nowPage'] = $nowPage;
 }
-
-function initPage($pageSize){
-	$_GET['pageSize'] = $pageSize;
-
-	$allPageNum = intval((getRecordNum() + $pageSize - 1) / $pageSize);
-	$_GET['allPageNum'] = $allPageNum;
-
+function initPage($pageSize) {
+	$_GET ['pageSize'] = $pageSize;
+	
+	$allPageNum = intval ( (getRecordNum () + $pageSize - 1) / $pageSize );
+	$_GET ['allPageNum'] = $allPageNum;
+	
 	$nowPage = 1;
-	if(isset($_GET['nowPage'])){
-		$nowPage = intval($_GET['nowPage']);
+	if (isset ( $_GET ['nowPage'] )) {
+		$nowPage = intval ( $_GET ['nowPage'] );
 	}
-	if($nowPage <= 0){
+	if ($nowPage <= 0) {
 		$nowPage = 1;
-	}else if($nowPage > $allPageNum){
+	} else if ($nowPage > $allPageNum) {
 		$nowPage = $allPageNum;
 	}
-	$_GET['nowPage'] = $nowPage;
+	$_GET ['nowPage'] = $nowPage;
 }
-
-function checkLogin(){
+function checkLogin() {
 	global $admin;
-	if(isset($_SESSION['record_admin'])){
-		$admin = $_SESSION['record_admin'];
-	}else{
+	if (isset ( $_SESSION ['record_admin'] )) {
+		$admin = $_SESSION ['record_admin'];
+	} else {
 		$admin = "";
 	}
 }
-
-function login(){
+function login() {
 	global $conn;
-	if(isset($_POST['username']) && isset($_POST['password']) && isset($_SESSION['verifyCode'])){
-		$username = mysql_real_escape_string($_POST['username']);
-		$verifyCode = mysql_real_escape_string($_POST['verifyCode']);
-		$password = sha1(SALT . $_POST['password']);
-
-		$_verifyCode = $_SESSION['verifyCode'];
+	if (isset ( $_POST ['username'] ) && isset ( $_POST ['password'] ) && isset ( $_SESSION ['verifyCode'] )) {
+		$username = mysql_real_escape_string ( $_POST ['username'] );
+		$verifyCode = mysql_real_escape_string ( $_POST ['verifyCode'] );
+		$password = sha1 ( SALT . $_POST ['password'] );
 		
-		$verifyCode = strtolower($verifyCode);
-		$_verifyCode = strtolower($_verifyCode);
-
-		if(strcmp($_verifyCode, $verifyCode) != 0){
-			return output(OUTPUT_ERROR,"验证码不正确");        
-		}else if($username == 'tiankonguse' && $password == 'dcd3eeb3b04e6d3fc4247bfec3a614ec86fe9db7' ){
-			$_SESSION['record_admin'] = 'record_admin';
-			return output(OUTPUT_SUCCESS,"");
-		}else{
-			return output(OUTPUT_ERROR,"你的用户名或密码不正确" );
+		$_verifyCode = $_SESSION ['verifyCode'];
+		
+		$verifyCode = strtolower ( $verifyCode );
+		$_verifyCode = strtolower ( $_verifyCode );
+		
+		if (strcmp ( $_verifyCode, $verifyCode ) != 0) {
+			return output ( OUTPUT_ERROR, "验证码不正确" );
+		} else if ($username == 'tiankonguse' && $password == 'dcd3eeb3b04e6d3fc4247bfec3a614ec86fe9db7') {
+			$_SESSION ['record_admin'] = 'record_admin';
+			return output ( OUTPUT_SUCCESS, "" );
+		} else {
+			return output ( OUTPUT_ERROR, "你的用户名或密码不正确" );
 		}
-	}else{
-		return output(OUTPUT_ERROR,"非法操作");
+	} else {
+		return output ( OUTPUT_ERROR, "非法操作" );
 	}
 }
-
-function write(){
+function write() {
 	global $conn;
-	if(isset($_POST['title']) && isset($_POST['time']) && isset($_POST['content'])){
-		//获得表单数据
-		$title    = $_POST['title'];
-		$time     = $_POST['time'];
-		$content  = $_POST['content'];
-		$tags     = $_POST['tags'];
-
-		//检查表单数据是否合法
-		if(strcmp($title,"") == 0 || strcmp($content,"") == 0){
-			return  output(OUTPUT_ERROR,"表单填写不完整");
+	if (isset ( $_POST ['title'] ) && isset ( $_POST ['time'] ) && isset ( $_POST ['content'] )) {
+		// 获得表单数据
+		$title = $_POST ['title'];
+		$time = $_POST ['time'];
+		$content = $_POST ['content'];
+		$tags = $_POST ['tags'];
+		
+		// 检查表单数据是否合法
+		if (strcmp ( $title, "" ) == 0 || strcmp ( $content, "" ) == 0) {
+			return output ( OUTPUT_ERROR, "表单填写不完整" );
 		}
-
-		if(strcmp($time,"") != 0){
-			//06/11/2013 00:00
-			sscanf($time,"%d/%d/%d %d:%d",$month, $day, $year, $hour, $minute);
+		
+		if (strcmp ( $time, "" ) != 0) {
+			// 06/11/2013 00:00
+			sscanf ( $time, "%d/%d/%d %d:%d", $month, $day, $year, $hour, $minute );
 			$second = 0;
-			$time = mktime($hour, $minute, $second ,$month ,$day, $year);
-		}else{
-			$time = time();
-			$year = date("Y",$time);
+			$time = mktime ( $hour, $minute, $second, $month, $day, $year );
+		} else {
+			$time = time ();
+			$year = date ( "Y", $time );
 		}
-		$last_time = time();
-
-
-
-		//防止sql注入
-		$title   = mysql_real_escape_string($title);
-		$content = mysql_real_escape_string($content);
-		$tags = mysql_real_escape_string($tags);
-
-		$tags = explode(",", $tags);
-
-		//操作数据库
+		$last_time = time ();
+		
+		// 防止sql注入
+		$title = mysql_real_escape_string ( $title );
+		$content = mysql_real_escape_string ( $content );
+		$tags = mysql_real_escape_string ( $tags );
+		
+		$tags = explode ( ",", $tags );
+		
+		// 操作数据库
 		$sql = "insert into `record_record` (`title`,`time`,`content`, `last_time`) values('$title','$time','$content','$last_time')";
-
-		$result = mysql_query($sql ,$conn);
-		if($result){
+		
+		$result = mysql_query ( $sql, $conn );
+		if ($result) {
 			$sql = "select id from `record_record` where time = '$time' and title = '$title'";
-			$result = mysql_query($sql ,$conn);
-			$row= mysql_fetch_array($result);
-			$id = $row['id'];
-			addTags($id, $tags);
-			return output(OUTPUT_SUCCESS,"添加成功");
-		}else{
-			return output(OUTPUT_ERROR,"数据库操作失败，请联系管理员");
+			$result = mysql_query ( $sql, $conn );
+			$row = mysql_fetch_array ( $result );
+			$id = $row ['id'];
+			addTags ( $id, $tags );
+			return output ( OUTPUT_SUCCESS, "添加成功" );
+		} else {
+			return output ( OUTPUT_ERROR, "数据库操作失败，请联系管理员" );
 		}
-	}else{
-		return output(OUTPUT_ERROR,"非法操作");
+	} else {
+		return output ( OUTPUT_ERROR, "非法操作" );
 	}
 }
-
-function addTags($recordId, $tags){
+function addTags($recordId, $tags) {
 	global $conn;
-
-	foreach($tags as $key=>$name){
+	
+	foreach ( $tags as $key => $name ) {
 		$sql = "select id from `record_tag` where name = '$name'";
-		$result = mysql_query($sql ,$conn);
-		$row= mysql_fetch_array($result);
-
-		$tagId = $row['id'];
-		if(!$row){
+		$result = mysql_query ( $sql, $conn );
+		$row = mysql_fetch_array ( $result );
+		
+		$tagId = $row ['id'];
+		if (! $row) {
 			$sql = "INSERT INTO `record_tag`(`name`) VALUES ('$name')";
-			mysql_query($sql ,$conn);
-            
-            $sql = "select id from `record_tag` where name = '$name'";
-			$result = mysql_query($sql ,$conn);
-			$row= mysql_fetch_array($result);
-		    $tagId = $row['id'];
+			mysql_query ( $sql, $conn );
+			
+			$sql = "select id from `record_tag` where name = '$name'";
+			$result = mysql_query ( $sql, $conn );
+			$row = mysql_fetch_array ( $result );
+			$tagId = $row ['id'];
 		}
-
-
+		
 		$sql = "INSERT INTO `record_tag_map`(`tag_id`, `record_id`) VALUES ('$tagId','$recordId')";
-		mysql_query($sql ,$conn);
-    
-        $sql = "update record_tag set number=numberr+1 where id in (select tag_id from record_tag_map where record_id = '$recordId')"; 
-    	mysql_query($sql ,$conn);
+		mysql_query ( $sql, $conn );
+		
+		$sql = "update record_tag set number=numberr+1 where id in (select tag_id from record_tag_map where record_id = '$recordId')";
+		mysql_query ( $sql, $conn );
 	}
 }
-
-function updateTags($recordId, $tags){
-    global $conn;
-
-    $sql = "update record_tag set number=number-1 where id in (select tag_id from record_tag_map where record_id = '$recordId')"; 
-	mysql_query($sql ,$conn);
-
-
+function updateTags($recordId, $tags) {
+	global $conn;
+	
+	$sql = "update record_tag set number=number-1 where id in (select tag_id from record_tag_map where record_id = '$recordId')";
+	mysql_query ( $sql, $conn );
+	
 	$sql = "delete from `record_tag_map` where record_id = '$recordId' ";
-	mysql_query($sql ,$conn);
-    
-    addTags($recordId, $tags);
+	mysql_query ( $sql, $conn );
+	
+	addTags ( $recordId, $tags );
 }
-
-function getTags($recordId){
+function getTags($recordId) {
 	global $conn;
 	$sql = "select name from `record_tag` where id in (select tag_id from record_tag_map where record_id = '$recordId')";
-	$result = mysql_query($sql ,$conn);
-	$res = array();
-	while($row= mysql_fetch_array($result)){
-		$res[] = $row["name"];
+	$result = mysql_query ( $sql, $conn );
+	$res = array ();
+	while ( $row = mysql_fetch_array ( $result ) ) {
+		if (strcmp ( $row ["name"], "" ) != 0) {
+			$res [] = $row ["name"];
+		}
 	}
 	return $res;
 }
-
-function getAllTags(){
+function getAllTags() {
 	global $conn;
 	$sql = "select name from `record_tag`";
-	$result = mysql_query($sql ,$conn);
-	$res = array();
-	while($row= mysql_fetch_array($result)){
-		$res[] = $row["name"];
+	$result = mysql_query ( $sql, $conn );
+	$res = array ();
+	while ( $row = mysql_fetch_array ( $result ) ) {
+		$res [] = $row ["name"];
 	}
 	return $res;
 }
-
-
-function alter(){
+function alter() {
 	global $conn;
-    
-    $staic = array(513,609,610);
-
-	if(isset($_POST['title']) && isset($_POST['time']) && isset($_POST['content'])){
-		//获得表单数据
-		$title    = $_POST['title'];
-		$time     = $_POST['time'];
-		$content  = $_POST['content'];
-		$id = $_SESSION['record_id'];
-		$tags     = $_POST['tags'];
-
-		//检查表单数据是否合法
-		if(strcmp($title,"") == 0 || strcmp($content,"") == 0){
-			return  output(OUTPUT_ERROR,"表单填写不完整");
+	
+	$staic = array (
+			513,
+			609,
+			610 
+	);
+	
+	if (isset ( $_POST ['title'] ) && isset ( $_POST ['time'] ) && isset ( $_POST ['content'] )) {
+		// 获得表单数据
+		$title = $_POST ['title'];
+		$time = $_POST ['time'];
+		$content = $_POST ['content'];
+		$id = $_SESSION ['record_id'];
+		$tags = $_POST ['tags'];
+		
+		// 检查表单数据是否合法
+		if (strcmp ( $title, "" ) == 0 || strcmp ( $content, "" ) == 0) {
+			return output ( OUTPUT_ERROR, "表单填写不完整" );
 		}
-
-
-		if(strcmp($time,"") != 0){
-			//06/11/2013 00:00
-			sscanf($time,"%d/%d/%d %d:%d",$month, $day, $year, $hour, $minute);
+		
+		if (strcmp ( $time, "" ) != 0) {
+			// 06/11/2013 00:00
+			sscanf ( $time, "%d/%d/%d %d:%d", $month, $day, $year, $hour, $minute );
 			$second = 0;
-			$time = mktime($hour, $minute, $second ,$month ,$day, $year);
-		}else{
-			$time = time();
-			$year = date("Y",$time);;
+			$time = mktime ( $hour, $minute, $second, $month, $day, $year );
+		} else {
+			$time = time ();
+			$year = date ( "Y", $time );
+			;
 		}
-        if(in_array($id, $staic)){
-            $last_time = $time;
-        }else{
-            $last_time = time();
-        }
-
-		//防止sql注入
-		$title   = mysql_real_escape_string($title);
-		$content = mysql_real_escape_string($content);
-		$tags = mysql_real_escape_string($tags);
-
-		$tags = explode(",", $tags);
-
-		//操作数据库
+		if (in_array ( $id, $staic )) {
+			$last_time = $time;
+		} else {
+			$last_time = time ();
+		}
+		
+		// 防止sql注入
+		$title = mysql_real_escape_string ( $title );
+		$content = mysql_real_escape_string ( $content );
+		$tags = mysql_real_escape_string ( $tags );
+		
+		$tags = explode ( ",", $tags );
+		
+		// 操作数据库
 		$sql = "UPDATE `record_record` SET `title`= '$title',`content`= '$content',`time`= '$time', `last_time` = '$last_time' WHERE `id` = '$id'";
-
-		$result = mysql_query($sql ,$conn);
-		if($result){
-			updateTags($id, $tags);
-			return output(OUTPUT_SUCCESS, "修改成功");
-		}else{
-			return output(OUTPUT_ERROR,"数据库操作失败，请联系管理员");
+		
+		$result = mysql_query ( $sql, $conn );
+		if ($result) {
+			updateTags ( $id, $tags );
+			return output ( OUTPUT_SUCCESS, "修改成功" );
+		} else {
+			return output ( OUTPUT_ERROR, "数据库操作失败，请联系管理员" );
 		}
-
-	}else{
-		return output(OUTPUT_ERROR,"非法操作");
+	} else {
+		return output ( OUTPUT_ERROR, "非法操作" );
 	}
 }
 
