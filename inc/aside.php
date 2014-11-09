1e17ad371731858829fbc46f5a0f326dc671a09d
tiankonguse <i@tiankonguse.com>
2014-11-09 15:31:58 +0800

    add robot.txt

--- inc/aside.php
+++ inc/aside.php
@@ -6,16 +6,7 @@
 				1.另一个博客地址:<a href="http://github.tiankonguse.com/">tiankonguse</a><br>
 				2.接受你的任何技术问题<br>
 				3.接受讲解任何算法<br>
-				4.接受租用广告位
 			</div>
-			<!-- 320-100 -->
-			<ins class="adsbygoogle"
-				 style="display:inline-block;width:320px;height:100px"
-				 data-ad-client="ca-pub-2326969899478823"
-				 data-ad-slot="9275075994"></ins>
-			<script>
-			(adsbygoogle = window.adsbygoogle || []).push({});
-			</script>
 		</li>
          <li>
             <h3>遗忘的页面</h3>
@@ -28,19 +19,8 @@
                 <a href="http://github.tiankonguse.com/project/format/" target="_blank"> 格式化/压缩代码</a><br>
                 <a href="http://github.tiankonguse.com/project/moondog/" target="_blank"> 纯CSS实现月食</a><br>
             </div>
-			
-            
-			<!-- 336-280 -->
-			<ins class="adsbygoogle"
-				 style="display:inline-block;width:336px;height:280px"
-				 data-ad-client="ca-pub-2326969899478823"
-				 data-ad-slot="4844876392"></ins>
-			<script>
-			(adsbygoogle = window.adsbygoogle || []).push({});
-			</script>
         </li>
         <li>
         </li>
     </ul>
 </div>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
--- record.php
+++ record.php
@@ -103,14 +103,6 @@ if (strcmp ( $admin, "record_admin" ) == 0) {
 
                         <section class="post" itemprop="articleBody">
                             <?php echo $content; ?>
-							<!-- 728-90 -->
-							<ins class="adsbygoogle"
-								 style="display:inline-block;width:728px;height:90px"
-								 data-ad-client="ca-pub-2326969899478823"
-								 data-ad-slot="1751809197"></ins>
-							<script>
-							(adsbygoogle = window.adsbygoogle || []).push({});
-							</script>
                         </section>
                         <section>
                         <?php require BASE_INC . "bdShare.php";?>
@@ -171,14 +163,6 @@ TK.loader.loadJS({url:"<?php echo PATH_JS;?>showImg.js"});
 TK.loader.loadJS({url:"<?php echo MAIN_PATH;?>js/showImg.js"});
 </script>
     </div>
-<!-- footer -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-2326969899478823"
-     data-ad-slot="5074793995"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 </body>
 </html>
 <?php require BASE_INC . "end.php";?>
